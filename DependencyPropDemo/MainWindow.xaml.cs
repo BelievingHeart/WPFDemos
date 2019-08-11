380bd12807871168b687e1d801dd1bb22673d20f
Afterburner <rockey1024@icloud.com>
2019-08-11 17:25:50 +0800

    Added multipropertiesTriggerDemo

---
+++ DependencyPropDemo/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace DependencyPropDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckBox_Click(object sender, RoutedEventArgs e)
+        {
+            var me = (CheckBox)sender;
+            if (me.IsChecked == null) return;
+            if ((bool)me.IsChecked)
+            {
+                StackPanel1.IsBackgroundPink = true;
+            }
+            else
+            {
+                StackPanel1.IsBackgroundPink = false;
+            }
+        }
+    }
+}
---
+++ DependencyPropDemo/MyStackPanel.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace DependencyPropDemo
+{
+    public class MyStackPanel : StackPanel
+    {
+        public MyStackPanel()
+        {
+            IsBackgroundPink = true;
+        }
+
+        public bool IsBackgroundPink
+        {
+            get { return (bool)GetValue(IsBackgroundPinkProperty); }
+            set { SetValue(IsBackgroundPinkProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty IsBackgroundPinkProperty =
+            DependencyProperty.Register("IsBackgroundPink", typeof(bool), typeof(MyStackPanel),
+                new PropertyMetadata(false, OnIsBackgroundPinkChanged));
+
+        private static object ValidateIsBackgroundPink(DependencyObject d, object basevalue)
+        {
+            var sender = (MyStackPanel) d;
+            if (sender.IsBackgroundPink)
+            {
+                MessageBox.Show("Color is about to change to pink");
+                return false;
+            }
+            else
+            {
+                MessageBox.Show("Color is about to change to orange");
+                return true;
+            }
+        }
+
+        private static void OnIsBackgroundPinkChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = (MyStackPanel) d;
+            if ((bool)e.NewValue)
+            {
+                sender.Background = Brushes.Pink;
+
+            }
+            else
+            {
+                sender.Background = Brushes.Orange;
+
+            }
+        }
+    }
+}
